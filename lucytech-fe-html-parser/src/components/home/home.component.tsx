3715678c9a99bfeef9d02aadfe17939d7c8cc3d1
Roshan G. Bolonna <srgbolonna@gmail.com>
2025-01-21 02:19:16 +0530

    done: status notification pop-up ui.

--- lucytech-fe-html-parser/src/components/home/home.component.tsx
+++ lucytech-fe-html-parser/src/components/home/home.component.tsx
@@ -1,10 +1,13 @@
-import {Button, Col, Input, Row} from "antd";
+import {Button, Col, Input, notification, Row} from "antd";
 import {Content} from "antd/es/layout/layout";
 import TextArea from "antd/es/input/TextArea";
 import {HtmlPageParserApi} from "../../api/html-parser-service/html-page-parser-api.ts";
 import {ChangeEvent, FC, useState} from "react";
 import {HtmlContextDetailsRequestDto} from "../../api/html-parser-service/dto/html-context-details.request.dto.ts";
 import Loader from "../notification/notification.component.tsx";
+import {AxiosError} from "axios";
+
+type NotificationType = 'success' | 'info' | 'warning' | 'error';
 
 const Home: FC = () => {
     const htmlPageParserApi: HtmlPageParserApi = new HtmlPageParserApi();
@@ -12,21 +15,42 @@ const Home: FC = () => {
     const [urlValue, setUrlValue] = useState<string>('');
     const [htmlResult, setHtmlResult] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
+    const [api, contextHolder] = notification.useNotification(
+        {
+            placement: 'topRight',
+            top: 50,
+            duration: 5,
+        }
+    );
+
+    // notification.config();
 
     const handleTxtUrlChange = (event: ChangeEvent<HTMLInputElement>) => {
         setUrlValue(event.target.value);
     }
+
+    const openNotificationWithIcon = (type: NotificationType, title: string, description: string) => {
+        api[type]({
+            message: title,
+            description: description,
+        });
+    };
 
     const handleBtnGetInfoClick = async () => {
         try {
             setLoading(true);
 
             const payload: HtmlContextDetailsRequestDto = new HtmlContextDetailsRequestDto(urlValue);
-            const response = await htmlPageParserApi.getHtmlMetadata(payload);
 
-            setHtmlResult(JSON.stringify(response, null, 2));
+            await htmlPageParserApi.getHtmlMetadata(payload).then(response => {
+                setHtmlResult(JSON.stringify(response, null, 2));
+                openNotificationWithIcon('success', 'Parsing Success', 'HTML parsing successfully completed...!');
+            }).catch((e: AxiosError) => {
+                openNotificationWithIcon('error', 'Error Parsing', e.response.data.payload.error);
+            });
 
-            console.log(htmlResult);
+        } catch (e) {
+            console.log(e);
         } finally {
             setLoading(false);
         }
@@ -34,6 +58,7 @@ const Home: FC = () => {
 
     return (
         <Content style={{position: 'relative', height: '100vh'}}>
+            {contextHolder}
             <Loader loading={loading}/>
             <Row justify="center" align="middle" style={{height: '100%'}}>
                 <Col span={18} style={{textAlign: 'center'}}>
